0ac7c1109920388ec2e9f438ea29c72d52ed87e0
[EMR]최다훈 <ekgns8258@douzone.com>
2026-02-25 15:54:44 +0900

    폰트 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import App from "./app/App";
 
 const root = createRoot(document.getElementById("root"));
